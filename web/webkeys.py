04228ec4c3732fdf2f842d569a97bf003dd3e099
lanming <1292510200@qq.com>
2022-04-01 16:41:42 +0800

    Update webkeys.py

--- web/webkeys.py
+++ web/webkeys.py
@@ -143,12 +143,36 @@ class  WebKey:
     def implicitly_wait(self,value=None):
         self.driver.implicitly_wait(value)
 
-    # def screenshot(self):
-    #     self.driver.get_screenshot_as_png()
+     def screenshot(self):
+         self.driver.get_screenshot_as_png()
 
     @allure.step
     def run_step(self,func, values):
         func(*values)
 
+        
+    # def logger(self):
+    #     self.logger = logging.getLogger(f"{self.id()}")  # id的方法
 
+    # def save_scree_image(self):
+    #     """
+    #     对当前页面进行截图
+    #     :return:
+    #     """
+    #     start_time = time.time()
+    #     filename = '{}.png'.format(start_time)
+    #     file_path = os.path.join(error_img, filename)
+    #     self.driver.save_screenshot(file_path)
+    #     log.info("错误页面截图成功，图表保存的路径:{}".format(file_path))
+    #     return file_path
+    #
+    # def save_image_to_allure(self):
+    #     """
+    #     保存失败的截图到allure报告中
+    #     :return:
+    #     """
+    #     file_path = self.save_scree_image()
+    #     with open(file_path, "rb") as f:
+    #         file = f.read()
+    #         allure.attach(file, "失败截图", allure.attachment_type.PNG)
 
